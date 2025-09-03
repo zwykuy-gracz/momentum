5eb8f458e8b86abfa3cf476f16c621371bf8b4e3
paul <dziurappa@gmail.com>
2025-09-03 14:27:32 +0200

    daily_update cleaned; yf_download..py ready - need to call it

--- 2025/daily_update.py
+++ 2025/daily_update.py
@@ -24,7 +24,7 @@ from utils import (
     previous_day,
     YTD_DATE,
     LAST_CORRECTION_DATE,
-    list_of_tickers_2B,
+    PREVIOUS_CORRECTION_DATE,
     list_of_tickers_5B,
     list_of_tickers_nasdaq,
     list_of_tickers_nyse,
@@ -61,10 +61,7 @@ class StockData(Base):
     date = Column(Date, nullable=False)
     ticker = Column(String, nullable=False, index=True)
     close = Column(Float, nullable=False)
-    high = Column(Float, nullable=False)
-    low = Column(Float, nullable=False)
     open = Column(Float, nullable=False)
-    volume = Column(Integer, nullable=False)
     ytd = Column(Integer, nullable=True)
     previous_correction = Column(Float, nullable=True)
     last_correction = Column(Float, nullable=True)
@@ -74,126 +71,9 @@ class StockData(Base):
     ma100_above = Column(Boolean, nullable=True)
     ma200 = Column(Float, nullable=True)
     ma200_above = Column(Boolean, nullable=True)
-    weekly_change = Column(Float, nullable=False)
 
     def __repr__(self):
         return f"<StockData(ticker='{self.ticker}', date='{self.date}', close={self.close})>"
-
-
-# copied but still needed here
-class TickersList5B(Base):
-    __tablename__ = "list_of_tickers_lt_5B"
-
-    id = Column(Integer, primary_key=True)
-    ticker = Column(String, nullable=False, index=True)
-    nasdaq_tickers = Column(Boolean, nullable=False)
-    nyse_tickers = Column(Boolean, nullable=False)
-
-    def __repr__(self):
-        return f"<StockPrice(ticker='{self.ticker}')>"
-
-
-# downloads from YF and write DFs to files
-def download_tickers_from_yf(tickers, last_date):
-    try:
-        fifth_length_of_tickers = len(tickers) // 5
-        df = yf.download(
-            tickers[:fifth_length_of_tickers],
-            group_by="Ticker",
-            start=last_date,
-            end=date.today(),
-        )
-        df = df.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index(level=1)
-        df = df.reset_index()
-        df = df.dropna(axis=1, how="all")
-        df.to_csv(
-            f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
-            index=False,
-        )
-
-        print("-------------------------------------")
-        print("One minute sleep during downloading from YF")
-        time.sleep(30)
-        print("30 more seconds")
-        time.sleep(30)
-        print("-------------------------------------")
-
-        for i in range(1, 5):
-            beginning = fifth_length_of_tickers * i
-            end = fifth_length_of_tickers * (i + 1)
-            df = yf.download(
-                tickers[beginning:end],
-                group_by="Ticker",
-                start=last_date,
-                end=date.today(),
-            )
-            df = df.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index(level=1)
-            df = df.reset_index()
-            df = df.dropna(axis=1, how="all")
-            df.to_csv(
-                f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
-                mode="a",
-                index=False,
-                header=False,
-            )
-
-            print("-------------------------------------")
-            print("One minute sleep during downloading from YF")
-            time.sleep(30)
-            print("30 more seconds")
-            time.sleep(30)
-            print("-------------------------------------")
-
-        if len(tickers) > fifth_length_of_tickers * 5:
-            print("last part")
-            df = yf.download(
-                tickers[fifth_length_of_tickers * 5 :],
-                group_by="Ticker",
-                start=last_date,
-                end=date.today(),
-            )
-            df = df.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index(level=1)
-            df = df.reset_index()
-            df = df.dropna(axis=1, how="all")
-            df.to_csv(
-                f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
-                mode="a",
-                index=False,
-                header=False,
-            )
-
-        print("YF tickers downloaded")
-        logging.info("YF API connection successful. Data downloaded.")
-    except Exception as e:
-        logging.error(f"YF API connection failed: {e}", exc_info=True)
-
-
-def read_df_from_csv_and_populate_db(last_date):
-    try:
-        df = pd.read_csv(
-            f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
-            engine="python",
-        )
-        df["Date"] = pd.to_datetime(df["Date"]).dt.date
-        logging.info(f"DF len: {len(df)}")
-
-        for _, row in df.iterrows():
-            stock_price = StockData(
-                date=row["Date"],
-                close=row["Close"],
-                high=row["High"],
-                low=row["Low"],
-                open=row["Open"],
-                volume=row["Volume"],
-                ticker=row["Ticker"],
-            )
-            session.add(stock_price)
-
-        session.commit()
-        print("DB Populated")
-        logging.info("Database successfully populated.")
-    except Exception as e:
-        logging.error(f"Database population failed: {e}", exc_info=True)
 
 
 def daily_count_new_records(last_date):
@@ -429,15 +309,9 @@ def check_above_below_sma(tickers, last_date):
 
 def main():
     try:
-        # copied
         print(f"Working on date: {previous_day}")
         logging.info(f"Working on date: {previous_day}")
-        # copied
-        # list_of_tickers_2B = creating_list_of_tickers_2B()
-        # list_of_tickers_5B = creating_list_of_tickers_5B()
 
-        download_tickers_from_yf(list_of_tickers_2B, previous_day)
-        read_df_from_csv_and_populate_db(previous_day)
         number_of_new_records_in_DB = daily_count_new_records(previous_day)
         if number_of_new_records_in_DB > 0:
             counting_and_populating_ytd_corrections_return(
@@ -468,12 +342,12 @@ def main():
         logging.critical(f"Critical error in main process: {e}", exc_info=True)
 
 
-# if __name__ == "__main__":
-# engine = create_engine(os.getenv("DB_ABSOLUTE_PATH"))
-# Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine(os.getenv("DB_ABSOLUTE_PATH"))
+    # Base.metadata.create_all(engine)
 
-# Session = sessionmaker(bind=engine)
-# session = Session()
-# main()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    main()
 
-# session.close()
+    session.close()
--- 2025/yf_download_db_population.py
+++ 2025/yf_download_db_population.py
@@ -1,6 +1,148 @@
 import yfinance as yf
-import logging
+import logging, time, os
+import pandas as pd
+from datetime import date
+from utils import previous_day, list_of_tickers_2B
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, Integer, Date, String, Float, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from utils import previous_day, YTD_DATE, LAST_CORRECTION_DATE
+load_dotenv()
 
-print(LAST_CORRECTION_DATE)
+logging.basicConfig(
+    filename=os.getenv("LOG_FILE"),
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+# engine = create_engine(os.getenv("DB_ABSOLUTE_PATH")) # prod
+engine = create_engine(os.getenv("DB_STOCK_DATA"))  # dev
+# Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class StockData(Base):
+    __tablename__ = "stock_data"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    ticker = Column(String, nullable=False, index=True)
+    close = Column(Float, nullable=False)
+    high = Column(Float, nullable=False)
+    low = Column(Float, nullable=False)
+    open = Column(Float, nullable=False)
+    volume = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<StockData(ticker='{self.ticker}', date='{self.date}', close={self.close})>"
+
+
+# downloads from YF and write DFs to files
+def download_tickers_from_yf(tickers, last_date):
+    try:
+        fifth_length_of_tickers = len(tickers) // 5
+        df = yf.download(
+            tickers[:fifth_length_of_tickers],
+            group_by="Ticker",
+            start=last_date,
+            end=date.today(),
+        )
+        df = df.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index(level=1)
+        df = df.reset_index()
+        df = df.dropna(axis=1, how="all")
+        df.to_csv(
+            f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
+            index=False,
+        )
+
+        print("-------------------------------------")
+        print("One minute sleep during downloading from YF")
+        time.sleep(30)
+        print("30 more seconds")
+        time.sleep(30)
+        print("-------------------------------------")
+
+        for i in range(1, 5):
+            beginning = fifth_length_of_tickers * i
+            end = fifth_length_of_tickers * (i + 1)
+            df = yf.download(
+                tickers[beginning:end],
+                group_by="Ticker",
+                start=last_date,
+                end=date.today(),
+            )
+            df = df.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index(level=1)
+            df = df.reset_index()
+            df = df.dropna(axis=1, how="all")
+            df.to_csv(
+                f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
+                mode="a",
+                index=False,
+                header=False,
+            )
+
+            print("-------------------------------------")
+            print("One minute sleep during downloading from YF")
+            time.sleep(30)
+            print("30 more seconds")
+            time.sleep(30)
+            print("-------------------------------------")
+
+        if len(tickers) > fifth_length_of_tickers * 5:
+            print("last part")
+            df = yf.download(
+                tickers[fifth_length_of_tickers * 5 :],
+                group_by="Ticker",
+                start=last_date,
+                end=date.today(),
+            )
+            df = df.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index(level=1)
+            df = df.reset_index()
+            df = df.dropna(axis=1, how="all")
+            df.to_csv(
+                f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
+                mode="a",
+                index=False,
+                header=False,
+            )
+
+        print("YF tickers downloaded")
+        logging.info("YF API connection successful. Data downloaded.")
+    except Exception as e:
+        logging.error(f"YF API connection failed: {e}", exc_info=True)
+
+
+def read_df_from_csv_and_populate_db(last_date):
+    try:
+        df = pd.read_csv(
+            f"{os.getenv('CSV_FOLDER_PATH')}/{str(last_date).replace('-', '')}.csv",
+            engine="python",
+        )
+        df["Date"] = pd.to_datetime(df["Date"]).dt.date
+        logging.info(f"DF len: {len(df)}")
+
+        for _, row in df.iterrows():
+            stock_price = StockData(
+                date=row["Date"],
+                close=row["Close"],
+                high=row["High"],
+                low=row["Low"],
+                open=row["Open"],
+                volume=row["Volume"],
+                ticker=row["Ticker"],
+            )
+            session.add(stock_price)
+
+        session.commit()
+        print("DB Populated")
+        logging.info("Database successfully populated.")
+    except Exception as e:
+        logging.error(f"Database population failed: {e}", exc_info=True)
+
+
+# download_tickers_from_yf(list_of_tickers_2B, previous_day)
+# read_df_from_csv_and_populate_db(previous_day)
